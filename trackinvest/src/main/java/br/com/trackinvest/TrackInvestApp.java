94d2968e7ba118810b8ffa7bdd9fa52163152fce
fabiosouza <fabiosouza.web@gmail.com>
2020-08-03 19:36:06 -0300

    Initial commit

---
+++ trackinvest/src/main/java/br/com/trackinvest/TrackInvestApp.java
@@ -0,0 +1,13 @@
+package br.com.trackinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackInvestApp 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(TrackInvestApp.class, args);
+    }
+}
---
+++ trackinvest/src/main/java/br/com/trackinvest/controller/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.trackinvest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController 
+{
+	@GetMapping("/")
+	public String home(Model model) {
+		
+		model.addAttribute("name", "teste");
+		return "home";
+	}
+}
