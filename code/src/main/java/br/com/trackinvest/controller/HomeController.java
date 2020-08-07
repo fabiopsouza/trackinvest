445760bd02c986914a725183eb8086cc1cd9f8b4
fabiosouza <fabiosouza.web@gmail.com>
2020-08-07 01:05:34 -0300

    Incluído thread e cache

--- code/src/main/java/br/com/trackinvest/controller/HomeController.java
+++ code/src/main/java/br/com/trackinvest/controller/HomeController.java
@@ -5,17 +5,20 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.text.ParseException;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
@@ -25,16 +28,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import br.com.trackinvest.model.Filter;
 import br.com.trackinvest.model.Result;
 import br.com.trackinvest.model.Yield;
+import br.com.trackinvest.scraping.Scraper;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class HomeController {	
 	
 	private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
-	private static final String URL_PRICE = "https://statusinvest.com.br/acoes/%s";
+	@Autowired
+	private Scraper scraper;
 	
-	private static final String URL_DIVIDEND = "https://www.fundamentus.com.br/proventos.php?papel=%s&tipo=2";
-
 	@GetMapping("/")
 	public String home(Model model) {
 
@@ -51,13 +56,15 @@ public class HomeController {
 	}
 
 	@PostMapping("/track")
-	public String track(Model model, Filter filter) throws IOException, ParseException {
+	public String track(Model model, Filter filter) throws IOException, ParseException, InterruptedException, ExecutionException {
 		
+		LocalDateTime startExecution = LocalDateTime.now();		
 		List<Yield> yields = new ArrayList<>();
-		Document page = Jsoup.connect(String.format(URL_DIVIDEND, filter.getStock())).get();
 
-		Elements lines = page.select("#resultado tbody tr");
-		for (Element line : lines) {
+		Future<Elements> futureDividends = scraper.scrapDividend(filter.getStock());
+		Future<Elements> futurePrice = scraper.scrapPrice(filter.getStock());
+		
+		for (Element line : futureDividends.get()) {
 		
 			Elements columns = line.select("td");
 			String date = columns.get(0).text();
@@ -89,13 +96,15 @@ public class HomeController {
 		}
 		
 		model.addAttribute("filter", filter);
-		model.addAttribute("result", calculate(yields, filter));
+		model.addAttribute("result", calculate(yields, filter, futurePrice));
 		model.addAttribute("yields", yields);
 
+		log.info(Duration.between(startExecution, LocalDateTime.now()).toMillis() + "");
+		
 		return "home";
 	}
 	
-	private Result calculate(List<Yield> yields, Filter filter) throws IOException {
+	private Result calculate(List<Yield> yields, Filter filter, Future<Elements> futurePrice) throws IOException, InterruptedException, ExecutionException {
 		
 		if(CollectionUtils.isEmpty(yields)) {
 			return null;
@@ -110,7 +119,7 @@ public class HomeController {
 		
 		result.setTotal(total);
 		result.setAverage(total.divide(BigDecimal.valueOf(yearGroup.size()), MathContext.DECIMAL128));
-		result.setPrice(getPrice(filter.getStock()));
+		result.setPrice(getPrice(futurePrice));
 		result.setAverageYield(result.getAverage().divide(result.getPrice(), MathContext.DECIMAL128).multiply(new BigDecimal("100")));
 		result.setLimitPrice(result.getAverage().divide(targetYield, MathContext.DECIMAL128));
 		result.setDifference(result.getLimitPrice().subtract(result.getPrice()));
@@ -123,13 +132,8 @@ public class HomeController {
 		return result;
 	}
 
-	private BigDecimal getPrice(String stock) throws IOException {
-		
-		Document page = Jsoup.connect(String.format(URL_PRICE, stock)).get();
-
-		Elements elements = page.select(".special .value");
-		String value = elements.get(0).text();
-		
+	private BigDecimal getPrice(Future<Elements> futurePrice) throws IOException, InterruptedException, ExecutionException {
+		String value = futurePrice.get().get(0).text();
 		return new BigDecimal(value.replace(',', '.'));
 	}
 
---
+++ code/src/main/java/br/com/trackinvest/scraping/Scraper.java
@@ -0,0 +1,35 @@
+package br.com.trackinvest.scraping;
+
+import java.io.IOException;
+import java.util.concurrent.Future;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scraper {
+
+	private static final String URL_PRICE = "https://statusinvest.com.br/acoes/%s";
+	
+	private static final String URL_DIVIDEND = "https://www.fundamentus.com.br/proventos.php?papel=%s&tipo=2";
+	
+	@Async
+	@Cacheable("dividends")
+	public Future<Elements> scrapDividend(String stock) throws IOException 
+	{
+		Document page = Jsoup.connect(String.format(URL_DIVIDEND, stock)).get();
+		return new AsyncResult<Elements>(page.select("#resultado tbody tr"));
+	}
+	
+	@Async
+	public Future<Elements> scrapPrice(String stock) throws IOException 
+	{
+		Document page = Jsoup.connect(String.format(URL_PRICE, stock)).get();
+		return new AsyncResult<Elements>(page.select(".special .value"));
+	}
+}
