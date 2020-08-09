9b2c45f6cd77bf349670c06fc403c25c04698f09
fabiosouza <fabiosouza.web@gmail.com>
2020-08-09 15:07:35 -0300

    Utilizado ponte para o scraping

--- code/src/main/java/br/com/trackinvest/service/ScrapService.java
+++ code/src/main/java/br/com/trackinvest/service/ScrapService.java
@@ -14,22 +14,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScrapService {
 
-	private static final String URL_PRICE = "https://statusinvest.com.br/acoes/%s";
+	private static final String URL_BRIDGE = "https://scraping-bridge.000webhostapp.com/?url=%s";
 	
 	private static final String URL_DIVIDEND = "https://www.fundamentus.com.br/proventos.php?papel=%s&tipo=2";
+	
+	private static final String URL_PRICE = "https://statusinvest.com.br/acoes/%s";
 	
 	@Async
 	@Cacheable("dividends")
 	public Future<Elements> scrapDividend(String stock) throws IOException 
 	{
-		Document page = Jsoup.connect(String.format(URL_DIVIDEND, stock)).get();
-		return new AsyncResult<Elements>(page.select("#resultado tbody tr"));
+		return scrap(URL_DIVIDEND, stock, "#resultado tbody tr");
 	}
 	
 	@Async
 	public Future<Elements> scrapPrice(String stock) throws IOException 
 	{
-		Document page = Jsoup.connect(String.format(URL_PRICE, stock)).get();
-		return new AsyncResult<Elements>(page.select(".special .value"));
+		return scrap(URL_PRICE, stock, ".special .value");
+	}
+	
+	public Future<Elements> scrap(String urlParameter, String stock, String selector) throws IOException 
+	{
+		String url = String.format(urlParameter, stock);
+		Document page = Jsoup.connect(String.format(URL_BRIDGE, url)).get();
+		return new AsyncResult<Elements>(page.select(selector));
 	}
 }
